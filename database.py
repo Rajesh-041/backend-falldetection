722aaab0a059423560834afb2f790205b57b8623
Muthu Rajesh T <muthurajesht041@gmail.com>
2026-03-04 04:46:43 +0500

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import datetime
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./data/fall_records.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./fall.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
